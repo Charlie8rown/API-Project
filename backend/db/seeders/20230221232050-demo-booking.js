8e3eb72ea0ac85a0108b6fbaa6df3c44ff37c94e
Charlie8rown <105841366+Charlie8rown@users.noreply.github.com>
2023-02-21 16:09:32 -0800

    updated seeders/demo-booking.js with test examples and added a bulkDelete in the async down

--- backend/db/seeders/20230221232050-demo-booking.js
+++ backend/db/seeders/20230221232050-demo-booking.js
@@ -12,6 +12,28 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+    options.tableName = 'Bookings';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 1,
+        startDate:"2023-3-25",
+        endDate:"2023-3-28",
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        startDate:"2023-10-27",
+        endDate:"2023-10-31",
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        startDate:"2023-12-25",
+        endDate:"2023-12-31",
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +43,13 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+
+    options.tableName = 'Bookings'
+    const Op = Sequelize.Op;
+
+    await queryInterface.bulkDelete(options, {
+      spotId:
+      { [Op.in]: [1, 2, 3] }
+    }, {})
   }
 };
