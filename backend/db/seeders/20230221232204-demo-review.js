a55cf48d1110d5e178e34da458b572f716ec7b97
Charlie8rown <105841366+Charlie8rown@users.noreply.github.com>
2023-02-21 16:11:55 -0800

    updated seeders/demo-re.js with test examples and added a bulkDelete in the async down

--- backend/db/seeders/20230221232204-demo-review.js
+++ backend/db/seeders/20230221232204-demo-review.js
@@ -12,6 +12,23 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+    options.tableName = 'ReviewImages';
+
+    return queryInterface.bulkInsert(options, [
+      {
+        reviewId: 1,
+        url: 'https://lh3.googleusercontent.com/p/AF1QipN9j_k1RDzMXTOQCkwGKrHWMQV-VVvnCPhHFbZ_=s680-w680-h510',
+      },
+      {
+        reviewId: 2,
+        url:'https://lh3.googleusercontent.com/p/AF1QipNYcYqhVHxcADlMcNczg4vDm_3tm9lfkn_5TbUz=s680-w680-h510',
+      },
+      {
+        reviewId: 3,
+        url:'https://lh3.googleusercontent.com/p/AF1QipOcNhDYREUzhUps9S4EOeIp0tnZtOxi9oU_h4qy=s680-w680-h510',
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +38,13 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+
+    options.tableName = 'ReviewImages'
+    const Op = Sequelize.Op;
+
+    await queryInterface.bulkDelete(options, {
+      reviewId:
+      { [Op.in]: [1, 2, 3] }
+    }, {})
   }
 };
