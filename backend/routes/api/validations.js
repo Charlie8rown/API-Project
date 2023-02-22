86c07b9009001a63e0f70f908711b2d09672dd61
Charlie8rown <105841366+Charlie8rown@users.noreply.github.com>
2023-02-22 15:39:46 -0800

    finished updating validations and still working on get all spots with pagination

--- backend/routes/api/validations.js
+++ backend/routes/api/validations.js
@@ -2,6 +2,60 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 
+
+// Handles errors if any of the attributes are empty for created spots and when editing a spot.
+const validateCreatedSpots = [
+  check('address')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Street address is required"),
+  check('city')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("City is required"),
+  check('state')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("State is required"),
+  check('country')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("Country is required"),
+  check('lat')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("Latitude is not valid"),
+  check('lng')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("Longitude is not valid"),
+  check('name')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("Name must be less than 50 characters"),
+  check('description')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("Description is required"),
+  check('price')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage("Price per day is required"),
+  handleValidationErrors
+]
+
+const validateBookings = [
+  check('startDate')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("A start date is required"),
+  check('endDate')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("An end date is required"),
+  handleValidationErrors
+]
+
 const validateQueryError = [
   check('page')
     .exists({ checkFalsy: true })
@@ -10,7 +64,20 @@ const validateQueryError = [
   check('size')
     .exists({ checkFalsy: true })
     .isInt({ min: 1 })
-    .withMessage("Size must be greater than or equal to 1")
+    .withMessage("Size must be greater than or equal to 1"),
+  handleValidationErrors
 ]
+
+const reviewValidateError = [
+  check('review')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Review text is required'),
+  check('stars')
+    .exists({ checkFalsy: true })
+    .withMessage('Stars must be an integer from 1 to 5'),
+  handleValidationErrors
+]
+
 
 module.exports = { validateCreatedSpots,  validateBookings, validateQueryError, reviewValidateError};
