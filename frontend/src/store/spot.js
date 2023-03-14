278d37eb0d2645a87cbc779e344c835cfe297669
Charlie8rown <105841366+Charlie8rown@users.noreply.github.com>
2023-03-13 23:27:02 -0700

    still working on get all spots and working a on a function to take in an array and return it as an object with the spot id as  akeyand an obj as a value so that I can access the data.

--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -8,10 +8,68 @@ const DELETE_SPOTS = "spts/deleteSpots";
 
 export const loadSpots = (Spots) => ({
   type: LOAD_SPOTS,
-  Spots
+  payload: spots
 })
 
 export const createSpots = (Spots) => ({
   type: ADD_SPOTS,
-  Spots
+  payload: spots
 })
+
+export const updateSpots = (spots) => ({
+  type: EDIT_SPOTS,
+  spots
+})
+
+export const removeSpots = () => ({
+  type: DELETE_SPOTS
+})
+
+export const getSpots = () => async (dispatch) => {
+  const response = await csrfFetch("/api/spot")
+}
+
+
+// Thunk Get all Spots
+export const getSpot = () => async (dispatch) => {
+  const response = await csrfFetch("/api/spots");
+  const data = await response.json();
+
+  dispatch(loadSpots(data))
+  return response
+}
+
+// Thunk Create Spot
+export const addSpot = (spots) => async (dispatch) => {
+  const {address, city, state, country, name, description, price} = spots
+  const response = await csrfFetch('/api/spots', {
+    method: "POST",
+    body: JSON.stringigy({
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price
+    })
+  })
+  const data = await response.json();
+
+  dispatch(createSpots(data.spots))
+  return response;
+}
+
+// Takes in an array and return an object with the spot id as a keya nd obj as a value.
+// I can access the data.
+const normalize = (spots) => {
+  const normalizeObj = {};
+
+  for (let i = 0; i < spots.length; i++) {
+    let spot = spots[i];
+    normalizeObj[spot.id] = spots
+  }
+  return normalizeObj
+}
