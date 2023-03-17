743ac8013c6e0dd177abe3572a8a8211346d1d92
Charlie8rown <105841366+Charlie8rown@users.noreply.github.com>
2023-03-16 17:08:16 -0700

    Finally get spot works will be moving to create a spot hopfully refactoring some old commits works or might try to do it from scratch if I run into any issues

--- frontend/src/components/SpotDetails/SpotDetails.js
+++ frontend/src/components/SpotDetails/SpotDetails.js
@@ -21,11 +21,11 @@ const SpotDetails = () => {
 
   return (
     <div>Hello there
-      {/* <h1>{spot.name}</h1>
+      <h1>{spot.name}</h1>
       <h2>{spot.city}, {spot.state}, {spot.country}</h2>
       <div>{spot.SpotImages.length > 0 ? (<img src={spot.SpotImages[0].url} alt="none" />) : (<img src={comingSoon} alt="" />)}</div>
       <div>{spot.SpotImages.length > 1 ? (<img src={spot.SpotImages[1].url} alt="none" />) : (<img src={comingSoon} alt="" />)}{spot.SpotImages.length > 2 ? (<img src={spot.SpotImages[2].url} alt=""/>) : (<img src={comingSoon} alt="" />)}</div>
-      <div>{spot.SpotImages.length > 3 ? (<img id="image-3" src={spot.SpotImages[3].url} alt="none" />) : (<img src={comingSoon} alt="" />)}{spot.SpotImages.length > 4 ? (<img id="image-4"src={spot.SpotImages[4].url} alt="none" />) : (<img src={comingSoon} alt="" />)}</div> */}
+      <div>{spot.SpotImages.length > 3 ? (<img id="image-3" src={spot.SpotImages[3].url} alt="none" />) : (<img src={comingSoon} alt="" />)}{spot.SpotImages.length > 4 ? (<img id="image-4"src={spot.SpotImages[4].url} alt="none" />) : (<img src={comingSoon} alt="" />)}</div>
     </div>
   );
 }
